668182158b6ac37d61898faaf966383762766b4e
cvetkoS <cvetko.stevic97@gmail.com>
2020-03-29 16:30:04 +0200

    Finished desktop navigation

--- dave-ceddia-challenges/challenge-1/src/js/index.js
+++ dave-ceddia-challenges/challenge-1/src/js/index.js
@@ -1,2 +1,9 @@
-const helloWorld = () => console.log('hello world');
-helloWorld();
+const createButton = document.querySelector('.js-button-create');
+const menu = document.querySelector('.js-nav-menu');
+
+function checkMenu(e) {
+	e.preventDefault();
+	menu.classList.contains('active') ? menu.classList.remove('active') : menu.classList.add('active');
+}
+
+createButton.addEventListener('click', checkMenu);
